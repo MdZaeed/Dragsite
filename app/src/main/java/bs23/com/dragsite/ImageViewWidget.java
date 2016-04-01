f4a3506f05b3e153d24d31119c547ee924cf95e9
MdZaeed <tz2201@gmail.com>
2016-04-01 21:01:06 +0600

    1st update

---
+++ app/src/main/java/bs23/com/dragsite/ImageViewWidget.java
@@ -0,0 +1,25 @@
+package bs23.com.dragsite;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+/**
+ * Created by BS-86 on 4/1/2016.
+ */
+public class ImageViewWidget extends LinearLayout {
+
+    Context context;
+    public ImageViewWidget(Context context) {
+        super(context);
+        this.context=context;
+    }
+
+    public void addContents()
+    {
+        super.addView(LayoutInflater.from(context).inflate(R.layout.image_view,null),new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                400));
+    }
+}
---
+++ app/src/main/java/bs23/com/dragsite/RecommnededStoreAdapter.java
@@ -0,0 +1,69 @@
+package bs23.com.dragsite;
+
+import android.content.ClipData;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Ashraful on 3/3/2016.
+ */
+public class RecommnededStoreAdapter extends RecyclerView.Adapter {
+    List<ElementsModel> elements;
+    protected Context context;
+
+    public RecommnededStoreAdapter(Context context, List<ElementsModel> elements) {
+        this.context = context;
+        this.elements = elements;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_add_elements, parent, false);
+        return new ElementsHolder(itemView);
+    }
+
+    public ElementsModel getItem(int position) {
+        return elements.get(position);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        ElementsHolder elementsHolder = (ElementsHolder) holder;
+        ElementsModel element = elements.get(position);
+        elementsHolder.elementName.setText(element.getElementName());
+        Drawable drawable=context.getResources().getDrawable(element.getImageId());
+        elementsHolder.elementName.setCompoundDrawablesWithIntrinsicBounds(null, drawable, null, null);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return elements.size();
+    }
+
+    protected class ElementsHolder extends RecyclerView.ViewHolder implements View.OnLongClickListener {
+        protected TextView elementName;
+
+        public ElementsHolder(View itemView) {
+            super(itemView);
+
+            elementName = (TextView) itemView.findViewById(R.id.tv_element_name);
+            itemView.setOnLongClickListener(this);
+        }
+
+        @Override
+        public boolean onLongClick(View v) {
+            ClipData data = ClipData.newPlainText("", "");
+            View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(v);
+            v.startDrag(data, shadowBuilder, v, 0);
+            return true;        }
+    }
+}

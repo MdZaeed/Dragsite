7e72817d18cdb0829406c1fa0a187a849e0525ed
Muntaqeem <muntaqeem@brainstation-23.com>
2016-04-04 14:18:34 +0600

    Layout and Class for TitleView added

---
+++ app/src/main/java/bs23/com/dragsite/TitleViewWidget.java
@@ -0,0 +1,25 @@
+package bs23.com.dragsite;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+/**
+ * Created by BrainStation on 4/4/16.
+ */
+public class TitleViewWidget extends LinearLayout {
+
+    Context context;
+
+    public TitleViewWidget(Context context) {
+        super(context);
+        this.context = context;
+    }
+    public void addContents()
+    {
+        super.addView(LayoutInflater.from(context).inflate(R.layout.title_view,null),new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+    }
+}

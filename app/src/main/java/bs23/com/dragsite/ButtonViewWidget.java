84f231ac5e477bda6b6302cf8ed7861b6353f969
Muntaqeem <muntaqeem@brainstation-23.com>
2016-04-04 15:32:16 +0600

    Layout for button and class added

---
+++ app/src/main/java/bs23/com/dragsite/ButtonViewWidget.java
@@ -0,0 +1,24 @@
+package bs23.com.dragsite;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+/**
+ * Created by BrainStation on 4/4/16.
+ */
+public class ButtonViewWidget extends LinearLayout {
+
+    Context context;
+    public ButtonViewWidget(Context context) {
+        super(context);
+        this.context=context;
+    }
+    public void addContents()
+    {
+        super.addView(LayoutInflater.from(context).inflate(R.layout.button_view,null),new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+    }
+}
